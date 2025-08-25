fe9a24020c92b1ab20047f6f441512f0ea45de5d
MyProjectBuddy <myprojectbuddyy@gmail.com>
2025-08-25 19:38:03 +0530

    deploy

--- ChatApp/src/App.jsx
+++ ChatApp/src/App.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { UserContextProvider } from "./UserContext.jsx";
 import Routes from "./Routes.jsx";
 
-axios.defaults.baseURL = "http://localhost:4040";
+axios.defaults.baseURL = "https://vaartalaap-qe02.onrender.com";
 axios.defaults.withCredentials = true;
 
 function App() {
